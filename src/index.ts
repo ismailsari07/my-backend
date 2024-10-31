625e5a4730d6548ef25e617bd0c419ef81dc66d0
ismailsari07 <xismail.sari@gmail.com>
2024-10-30 22:22:46 -0400

    sign up yazilid

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import {User} from "./model";
+import {Output, User} from "./model";
 
 const cors = require('cors');
 const app = express();
+const bcrypt = require('bcrypt');
 const db = require('./db');
 const port = 3000;
 
@@ -31,25 +32,56 @@ app.get('/GetUsers', async (req: Request, res: Response) => {
   });
   
 app.post('/SetUser', async (req: Request, res: Response) => {
-     const { isThereUser, userName, country, phoneNumber, email, password } = req.body;
+    const { userName, country, phoneNumber, email, password } = req.body;
+    let result: Output;
 
-    // res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-    // res.setHeader("Access-Control-Max-Age", "1800");
-    // res.setHeader("Access-Control-Allow-Headers", "content-type");
-    // res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     try {
-        const result = await db.query(`INSERT INTO users (username, email, password_hash, phone_number, country, last_login)
-        VALUES ('${userName}', '${email}', '${password}', '${phoneNumber}', '${country}', current_timestamp);`);
-        res.json(result.rows);
+        const resultQuery = await db.query(`INSERT INTO users (username, email, password, phone_number, country, last_login)
+            VALUES ('${userName}', '${email}', '${hashedPassword}', '${phoneNumber}', '${country}', current_timestamp);`);
+
+        result = {Response: {}, Status: resultQuery.rowCount === 1, Error: []};
+        res.json(result);
       } catch (err) {
-        console.error(err);
+        result = {Response: {}, Status: false, Error: []};
+        res.json(result);
         res.status(500).send('Internal Server Error');
       }
-
-      //res.status(201).json({message: "good job, you did it!"});
 });
+
+app.post('/Register', async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    let result: Output;
+    debugger;
+
+    try {
+        const resultQuery = await db.query(`SELECT PASSWORD FROM USERS WHERE EMAIL = '${email}';`)
+
+        if (resultQuery.rowCount === 1) {
+            const isMatch = await bcrypt.compare(password, resultQuery.rows[0].password);
+
+            if (isMatch) {
+                result = {Response: {}, Status: true, Error: []};
+                res.json(result);
+            }
+            else {
+                result = {Response: {}, Status: false, Error: [{ErrorCode: 493974, ErrorMessage: 'Email Or Password were wrong'}]};
+                res.json(result);
+            }
+        }
+        else {
+            result = {Response: {}, Status: false, Error: [{ErrorCode: 493973, ErrorMessage: 'Email Or Password were wrong'}]};
+            res.json(result);
+        }
+      } catch (err) {
+        result = {Response: {}, Status: false, Error: [{ErrorCode: 404, ErrorMessage: 'SERVER ERROR'}]};
+        res.json(result);
+        res.status(500).send('Internal Server Error');
+      }
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,14 @@ export interface User {
     name: string, 
     email: string
 }
+
+export interface Output {
+    Response: object,
+    Error: Array<Error>,
+    Status: boolean
+}
+
+export interface Error {
+    ErrorMessage: string,
+    ErrorCode: number
+}
